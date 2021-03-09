60404ed6d90faac9c969d98aae7851efa1dafd4e
Douglas Barcellos <douglasbarcellosti@gmail.com>
2021-03-09 18:17:06 -0300

    Finalizada e incluido JS

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
- 
+function calculateTip(event){
+    event.preventDefault();
+    var bill = document.getElementById('bill').value;
+    var serviceQual = document.getElementById('serviceQual').value;
+    var numOfPeople = document.getElementById('people').value;
+
+    if(bill == '' | serviceQual == 0){
+        alert("Por favor, preencha os valores")
+        return;
+    }
+
+    if(numOfPeople == "" | numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "none"
+    }else {
+        document.getElementById('each').style.display = "block"
+    }
+    
+    var total = (bill * serviceQual) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = "block";
+}
+
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
